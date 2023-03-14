dd4da4ae218504750246a41631439132f195b6d0
younghwan12 <dowk3432@gmail.com>
2023-03-14 18:14:25 +0900

    베이스 레이아웃 세팅

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import getConfig from 'next/config';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const contextPath = getConfig().publicRuntimeConfig.contextPath;
+
+    console.log("contextPath", getConfig().publicRuntimeConfig)
+
+    return (
+      <Html lang="en">
+        <Head>
+          <link id="theme-css" href={`${contextPath}/themes/lara-light-indigo/theme.css`} rel="stylesheet"></link>
+          {/*<link id="theme-css" href={`${contextPath}/themes/lara-light-purple/theme.css`} rel="stylesheet"></link> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
